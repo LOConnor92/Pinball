5b65eedd086cab1122fc73b32e7a46b27f540582
AngelAceGaming <ldgjpoconnor@gmail.com>
2015-03-10 01:22:49 +0000

    Inital upload
    First upload. All I have done is the script to move the paddle, a script
    to follow the ball, and a material for the ball

---
+++ Assets/Scripts/FlipperController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipperController : MonoBehaviour 
+{
+	public KeyCode press;
+	public enum Angle{x, y, z};
+	public Angle angle = Angle.x;
+	public bool antiClockwise = false;
+	public float maxRot;
+	public float speed;
+
+	Quaternion flipRot;
+	Quaternion startRot;
+
+	void Start()
+	{
+		startRot = transform.rotation;
+		flipRot = Quaternion.Euler(transform.rotation.eulerAngles + WorkOutFlipRot());
+	}
+
+	Vector3 WorkOutFlipRot()
+	{
+		Vector3 ret = Vector3.zero;
+		switch (angle) 
+		{
+		case Angle.x:
+			ret = Vector3.right * maxRot;
+			break;
+		case Angle.y:
+			ret = Vector3.up * maxRot;
+			break;
+		case Angle.z:
+			ret = Vector3.forward * maxRot;
+			break;
+		}
+
+		if (antiClockwise)
+			ret = -ret;
+
+		return ret;
+	}
+
+	void Update()
+	{
+		CanMove (Input.GetKey (press));
+	}
+
+	void CanMove(bool check)
+	{
+		if (check) {
+			if (Mathf.Abs (Vector3.Distance (transform.rotation.eulerAngles, flipRot.eulerAngles)) > 0.01f)
+				transform.rotation = Quaternion.Slerp (transform.rotation, flipRot, Time.deltaTime * speed);
+			else
+				transform.rotation = flipRot;
+		}
+		else 
+		{
+			if (Mathf.Abs (Vector3.Distance (transform.rotation.eulerAngles, startRot.eulerAngles)) > 0.01f)
+				transform.rotation = Quaternion.Slerp (transform.rotation, startRot, Time.deltaTime * speed);
+			else
+				transform.rotation = startRot;
+		}
+	}
+}
---
+++ Assets/Scripts/FollowBall.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowBall : MonoBehaviour
+{
+	public Transform ball;
+	public float height;
+	public float minPos;
+	public float maxPos;
+
+	Vector3 pos;
+
+	void Start()
+	{
+		pos = transform.position;
+	}
+
+	void FixedUpdate()
+	{
+		if (pos.z != Mathf.Clamp (ball.position.z, minPos, maxPos))
+			pos.z = Mathf.Clamp(ball.position.z, minPos, maxPos);
+		if (transform.position != Vector3.Lerp (transform.position, pos, Time.deltaTime))
+			transform.position = Vector3.Lerp (transform.position, pos, Time.deltaTime);
+	}
+}
